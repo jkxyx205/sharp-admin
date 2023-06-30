96a7f2514cc3218a731cec7556b0f3f61088c389
rick <jkxyx205@163.com>
2023-06-30 17:34:32 +0800

    chore: 添加sharp-database 省略 DAO 模版代码的支持

--- src/main/java/com/rick/admin/module/core/dao/PlantDAO.java
+++ src/main/java/com/rick/admin/module/core/dao/PlantDAO.java
@@ -2,13 +2,14 @@ package com.rick.admin.module.core.dao;
 
 import com.rick.admin.module.core.entity.Plant;
 import com.rick.db.plugin.dao.core.EntityCodeDAOImpl;
-import org.springframework.stereotype.Repository;
 
 /**
  * @author Rick.Xu
  * @date 2023/6/1 11:13
  */
-@Repository
+//@Repository
+// 自动扫描实现，手动继承 EntityCodeDAOImpl
+@Deprecated
 public class PlantDAO extends EntityCodeDAOImpl<Plant, Long> {
 
 }
