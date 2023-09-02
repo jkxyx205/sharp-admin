a411930978685f20f53d0c5fb4bbf232131ac11f
rick <jkxyx205@163.com>
2023-09-02 14:28:30 +0800

    fix: bugfix

--- src/main/resources/static/js/editable-table-plus.js
+++ src/main/resources/static/js/editable-table-plus.js
@@ -368,7 +368,8 @@
 
                     // 注册事件
                     $input.on('keydown', function(event){
-                        if ((event.keyCode > 57 || event.keyCode < 48) && event.keyCode !== 8 && event.keyCode !== 190 && event.keyCode !== 39 && event.keyCode !== 37 && event.keyCode !== 9) {
+                        // if ((event.keyCode > 57 || event.keyCode < 48) && event.keyCode !== 8 && event.keyCode !== 190 && event.keyCode !== 39 && event.keyCode !== 37 && event.keyCode !== 9) {
+                        if (!((event.keyCode >= 48 && event.keyCode <= 57) || (event.keyCode >= 96 && event.keyCode <= 105) || event.keyCode === 37 || event.keyCode === 39 || event.keyCode === 8 || event.keyCode === 9 || event.keyCode === 110 || event.keyCode === 190)) {
                             event.preventDefault();
                             event.stopPropagation()
                             return false;
