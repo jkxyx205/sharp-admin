283036937ac9e6daa06ed87fb0b4244efe469bed
rick <jkxyx205@163.com>
2023-09-04 13:28:36 +0800

    chore: 功能增强

--- src/main/java/com/rick/admin/module/inventory/controller/CountController.java
+++ src/main/java/com/rick/admin/module/inventory/controller/CountController.java
@@ -2,6 +2,7 @@ package com.rick.admin.module.inventory.controller;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.rick.admin.common.api.ExcelCellStyleHelper;
 import com.rick.admin.module.core.entity.Classification;
 import com.rick.admin.module.material.dao.ClassificationDAO;
 import com.rick.common.http.HttpServletResponseUtils;
@@ -18,21 +19,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.poi.ss.usermodel.BorderStyle;
-import org.apache.poi.ss.usermodel.FillPatternType;
-import org.apache.poi.ss.usermodel.HorizontalAlignment;
-import org.apache.poi.ss.usermodel.VerticalAlignment;
-import org.apache.poi.xssf.usermodel.*;
-import org.apache.poi.xssf.usermodel.extensions.XSSFCellBorder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.awt.*;
 import java.io.IOException;
-import java.util.List;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -62,12 +55,12 @@ public class CountController {
 
         ExcelCell title = new ExcelCell(1, 1, "盘点单", 1, 7);
         title.setHeightInPoints(heightInPoints);
-        title.setStyle(createTitleStyle(excelWriter.getBook()));
+        title.setStyle(ExcelCellStyleHelper.createTitleStyle(excelWriter.getBook()));
         excelWriter.writeCell(title);
 
         ExcelRow label = new ExcelRow(1, 2, new Object[]{"分类", "物料编号", "名称", "规格", "特征值", "单位", "数量"});
         label.setHeightInPoints(heightInPoints);
-        label.setStyle(createLabelStyle(excelWriter.getBook()));
+        label.setStyle(ExcelCellStyleHelper.createLabelStyle(excelWriter.getBook()));
         excelWriter.writeRow(label);
 
         excelWriter.getActiveSheet().setColumnWidth(0, 4000);
@@ -112,7 +105,7 @@ public class CountController {
             }
 
             ExcelCell categoryCell = new ExcelCell(1, y, key.substring(key.indexOf("-") + 1), valueSize + characteristicValueListSize - classificationListNumber, 1);
-            categoryCell.setStyle(createCellStyle(excelWriter.getBook()));
+            categoryCell.setStyle(ExcelCellStyleHelper.createCellStyle(excelWriter.getBook()));
             excelWriter.writeCell(categoryCell);
 
             for (int i = 0; i < valueSize; i++) {
@@ -127,7 +120,7 @@ public class CountController {
                                 dictService.getDictByTypeAndName("unit", (String) value.get("base_unit")).get().getLabel(), ""
                         });
                         dataRow.setHeightInPoints(heightInPoints);
-                        dataRow.setStyle(createCellStyle(excelWriter.getBook()));
+                        dataRow.setStyle(ExcelCellStyleHelper.createCellStyle(excelWriter.getBook()));
                         excelWriter.writeRow(dataRow);
 
                         y++;
@@ -140,7 +133,8 @@ public class CountController {
                             dictService.getDictByTypeAndName("unit", (String) value.get("base_unit")).get().getLabel(), ""
                     });
                     dataRow.setHeightInPoints(heightInPoints);
-                    dataRow.setStyle(createCellStyle(excelWriter.getBook()));
+
+                    dataRow.setStyle(ExcelCellStyleHelper.createCellStyle(excelWriter.getBook()));
                     excelWriter.writeRow(dataRow);
 
                     y++;
@@ -158,80 +152,6 @@ public class CountController {
         return CollectionUtils.isNotEmpty(specificationList) ? specificationList.stream().map(list -> list.get(1).toString()).collect(Collectors.joining("/")) : "";
     }
 
-    private XSSFCellStyle createCellStyle(XSSFWorkbook book) {
-        XSSFCellStyle cellStyle = book.createCellStyle();
-
-        // 垂直居中
-        cellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
-        return cellStyle;
-    }
-
-    private XSSFCellStyle createLabelStyle(XSSFWorkbook book) {
-        XSSFCellStyle cellStyle = book.createCellStyle();
-        XSSFColor color = new XSSFColor(Color.lightGray, new DefaultIndexedColorMap());
-        // 填充色
-        cellStyle.setFillForegroundColor(color);
-        cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-
-        // 垂直居中
-        cellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
-
-        // 文字
-        XSSFFont font = book.createFont();
-        font.setBold(true);
-        cellStyle.setFont(font);
-        return cellStyle;
-    }
-
-    private XSSFCellStyle createTitleStyle(XSSFWorkbook book) {
-        XSSFCellStyle cellStyle = book.createCellStyle();
-        XSSFColor color = new XSSFColor(Color.lightGray, new DefaultIndexedColorMap());
-        // 填充色
-        cellStyle.setFillForegroundColor(color);
-        cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-
-        // 水平居中
-        cellStyle.setAlignment(HorizontalAlignment.CENTER);
-        // 垂直居中
-        cellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
-
-        // 文字
-        XSSFFont font = book.createFont();
-        font.setBold(true);
-        font.setFontHeight(23);
-        cellStyle.setFont(font);
-        return cellStyle;
-    }
-
-    private XSSFCellStyle createStyle(XSSFWorkbook book) {
-        XSSFCellStyle cellStyle = book.createCellStyle();
-
-        // 定义颜色
-        XSSFColor color = new XSSFColor(java.awt.Color.BLUE, new DefaultIndexedColorMap());
-        XSSFColor color2 = new XSSFColor(Color.RED, new DefaultIndexedColorMap());
-        XSSFColor color3 = new XSSFColor(Color.GREEN, new DefaultIndexedColorMap());
-
-        // 填充色
-        cellStyle.setFillForegroundColor(color2);
-        cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-
-        // 文字色
-        XSSFFont font = book.createFont();
-        font.setColor(color);
-        font.setFontHeight(23);
-        cellStyle.setFont(font);
-
-        // 设置边框(合并这个不生效) 需要单独在CellRangeAddress设置
-        cellStyle.setBorderBottom(BorderStyle.MEDIUM);
-        cellStyle.setBorderColor(XSSFCellBorder.BorderSide.BOTTOM, color3);
-
-        // 水平居中
-        cellStyle.setAlignment(HorizontalAlignment.CENTER);
-        // 垂直居中
-        cellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
-
-        return cellStyle;
-    }
 
     private void getCharacteristicValueList(List<String> characteristicValueList, List<List<String>> data, int index, String value) {
         if (index >= data.size()) {
--- src/test/java/com/rick/admin/module/StockWarningTest.java
+++ src/test/java/com/rick/admin/module/StockWarningTest.java
@@ -31,7 +31,7 @@ public class StockWarningTest {
                 .name("安全库存预警")
                 .additionalInfo(Params.builder(1).pv("formId", "695978675677433856").build())
                 .reportAdviceName("stockWarningReportAdvice")
-                .querySql("select id, code, name, `specification`, mm_material.category_id, ifnull(stock.quantity, 0) stock_quantity, `safety_stock_quantity`, `maximum_stock_quantity` from mm_material left join (select material_id, sum(quantity) quantity from inv_stock group by material_id) stock on stock.material_id = mm_material.id where ((safety_stock_quantity is not null and safety_stock_quantity > ifnull(stock.quantity, 0))  or (maximum_stock_quantity is not null and maximum_stock_quantity < ifnull(stock.quantity, 0))) AND mm_material.id = :material_id AND category_id = :categoryId")
+                .querySql("select id, code, name, `specification`, mm_material.category_id, ifnull(stock.quantity, 0) stock_quantity, `safety_stock_quantity`, `maximum_stock_quantity` from mm_material left join (select material_id, sum(quantity) quantity from inv_stock where plant_id = 719893335619162112 group by material_id) stock on stock.material_id = mm_material.id where ((safety_stock_quantity is not null and safety_stock_quantity > ifnull(stock.quantity, 0))  or (maximum_stock_quantity is not null and maximum_stock_quantity < ifnull(stock.quantity, 0))) AND mm_material.id = :material_id AND category_id = :categoryId")
                 .queryFieldList(Arrays.asList(
                         new QueryField("categoryId", "分类", QueryField.Type.SELECT, "core_material_category")
                 ))
@@ -41,7 +41,7 @@ public class StockWarningTest {
                         new ReportColumn("name", "名称", true),
                         new ReportColumn("specification", "规格", false, null, Arrays.asList("characteristicConverter")),
                         new ReportColumn("category_path", "分类", false),
-                        new ReportColumn("stock_quantity", "库存").setType(ReportColumn.TypeEnum.NUMERIC).setAlign(AlignEnum.RIGHT),
+                        new ReportColumn("stock_quantity", "材料库库存").setType(ReportColumn.TypeEnum.NUMERIC).setAlign(AlignEnum.RIGHT),
                         new ReportColumn("safety_stock_quantity", "安全库存").setType(ReportColumn.TypeEnum.NUMERIC).setAlign(AlignEnum.RIGHT),
                         new ReportColumn("maximum_stock_quantity", "最大库存").setType(ReportColumn.TypeEnum.NUMERIC).setAlign(AlignEnum.RIGHT)
                 ))
