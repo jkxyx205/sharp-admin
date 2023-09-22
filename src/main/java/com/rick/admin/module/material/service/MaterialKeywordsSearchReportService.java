21a84a31ef4814d0a7246f253af1f7a58d3bced4
rick <jkxyx205@163.com>
2023-09-22 14:38:07 +0800

    chore: 物料关键字搜索支持「特征值」

--- src/main/java/com/rick/admin/module/material/service/MaterialKeywordsSearchReportService.java
+++ src/main/java/com/rick/admin/module/material/service/MaterialKeywordsSearchReportService.java
@@ -23,7 +23,9 @@ public class MaterialKeywordsSearchReportService implements ReportAdvice {
             String condition = "";
             for (int i = 0; i < keywordArray.length; i++) {
                 String paramName = "keyword" + i;
-                condition += " AND (mm_material.code like :"+paramName+" or mm_material.name like :"+paramName+" or specification like :"+paramName+")";
+                condition += " AND (mm_material.code like :"+paramName+" or mm_material.name like :"+paramName+" or specification like :"+paramName+" " +
+                        "OR (batch_id in (select mm_batch.id from mm_batch, mm_profile, mm_characteristic_value where\n" +
+                        " mm_batch.profile_id = mm_profile.id AND mm_characteristic_value.reference_id = mm_profile.id AND mm_characteristic_value.value like :"+paramName+")))";
                 requestMap.put(paramName, keywordArray[i]);
             }
 
