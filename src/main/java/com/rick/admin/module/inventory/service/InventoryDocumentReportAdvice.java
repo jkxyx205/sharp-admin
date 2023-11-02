f4c23a56e80859e630541dca4fce316864ebf6ec
rick <jkxyx205@163.com>
2023-11-02 18:24:59 +0800

    refactor: code optimize

--- src/main/java/com/rick/admin/module/inventory/service/InventoryDocumentReportAdvice.java
+++ src/main/java/com/rick/admin/module/inventory/service/InventoryDocumentReportAdvice.java
@@ -5,6 +5,7 @@ import com.rick.admin.module.material.entity.Material;
 import com.rick.admin.module.material.service.CharacteristicConverter;
 import com.rick.admin.module.material.service.MaterialProfileService;
 import com.rick.admin.module.material.service.MaterialProfileSupport;
+import com.rick.db.service.support.Params;
 import com.rick.report.core.entity.Report;
 import com.rick.report.core.service.ReportAdvice;
 import lombok.AllArgsConstructor;
@@ -33,17 +34,24 @@ public class InventoryDocumentReportAdvice implements ReportAdvice {
 
     @Override
     public void beforeSetRow(Report report, List<Map<String, Object>> rows) {
-        Map<Long, Material> idMaterialMap = materialDAO.selectByIdsAsMap(rows.stream().map(row -> (Long) row.get("material_id")).collect(Collectors.toSet()));
+//        Map<Long, Material> idMaterialMap = materialDAO.selectByIdsAsMap(rows.stream().map(row -> (Long) row.get("material_id")).collect(Collectors.toSet()));
+
+        // is_deleted = :tmp 给条件但是没有赋值作用是查询不需要带 is_deleted = 0， 而是查询所有的记录
+        Map<Long, Material> idMaterialMap = materialDAO.selectByIdsAsMap(Params.builder(1).pv("ids", rows.stream().map(row -> (Long) row.get("material_id")).collect(Collectors.toSet())).build(),
+                "id IN (:ids) AND is_deleted = :tmp");
         Set<String> materialIdBatchIdStringCollection = rows.stream().map(row -> MaterialProfileSupport.materialIdBatchIdString((Long) row.get("material_id"), (Long) row.get("batch_id"))).collect(Collectors.toSet());
         Map<String, String> characteristicTextMap = materialProfileService.getCharacteristicText(materialIdBatchIdStringCollection);
 
         for (Map<String, Object> row : rows) {
             Material material = idMaterialMap.get((Long) row.get("material_id"));
-            row.put("material_code", material.getCode());
-            row.put("materialName", material.getName());
-            row.put("materialSpecification", material.getSpecificationText());
-            row.put("quantity", (Objects.equals(row.get("movement_type"), "INBOUND") ? "+" : "-") + row.get("quantity"));
-            row.put("characteristic", characteristicTextMap.get(MaterialProfileSupport.materialIdBatchIdString(material.getId(), (Long) row.get("batch_id"))));
+            if (Objects.nonNull(material)) {
+                row.put("material_code", material.getCode());
+                row.put("materialName", material.getName());
+                row.put("materialSpecification", material.getSpecificationText());
+                row.put("quantity", (Objects.equals(row.get("movement_type"), "INBOUND") ? "+" : "-") + row.get("quantity"));
+                row.put("characteristic", characteristicTextMap.get(MaterialProfileSupport.materialIdBatchIdString(material.getId(), (Long) row.get("batch_id"))));
+            }
+
             String specification = (String) row.get("materialSpecification");
             row.put("specificationAndCharacteristic", (StringUtils.isBlank(specification) ? "" : specification + " ") + Objects.toString(row.get("characteristic"), ""));
 
