5d1e3f81129b9bb6c79a12312b20702cac445180
rick <jkxyx205@163.com>
2023-11-17 17:51:01 +0800

    chore: 功能优化

--- src/main/java/com/rick/admin/module/core/service/CodeHelper.java
+++ src/main/java/com/rick/admin/module/core/service/CodeHelper.java
@@ -2,8 +2,10 @@ package com.rick.admin.module.core.service;
 
 import com.rick.common.util.Time2StringUtils;
 import lombok.experimental.UtilityClass;
+import org.apache.commons.lang3.RandomStringUtils;
 
 import java.time.Instant;
+import java.util.function.Function;
 
 /**
  * @author Rick.Xu
@@ -13,10 +15,17 @@ import java.time.Instant;
 public class CodeHelper {
 
     public String generateCode(String prefix) {
-        return prefix + Time2StringUtils.format(Instant.now()).replaceAll("\\s+|-|:", "");
+        return prefix + getTimestamp(time -> time.replaceAll("\\s+|-|:", ""));
     }
 
     public String generateCodeWithDay(String prefix) {
-        return prefix + Time2StringUtils.format(Instant.now()).replaceAll("\\s+|-|:", "").substring(0, 8);
+        return prefix + getTimestamp(time -> time.replaceAll("\\s+|-|:", "").substring(0, 8));
     }
+
+    private String getTimestamp(Function<String, String> formatTimestamp) {
+        String timestamp = formatTimestamp.apply(Time2StringUtils.format(Instant.now()));
+        timestamp = String.valueOf(Long.parseLong(timestamp) + Long.parseLong(RandomStringUtils.randomNumeric(5)));
+        return timestamp;
+    }
+
 }
--- src/test/java/com/rick/admin/SimpleTest.java
+++ src/test/java/com/rick/admin/SimpleTest.java
@@ -23,7 +23,7 @@ public class SimpleTest {
 
     @Test
     public void test() {
-        String sql = " SELECt mm_material.id, mm_material.code, mm_material.name, specification, case when attachment is null or length(attachment) <= 2 then '无' else '有' end  attachment, material_type, mm_material.category_id, base_unit, standard_price, sys_user.name create_name,DATE_FORMAT(mm_material.create_time, '%Y-%m-%d %H:%i:%s') create_time from mm_material left join sys_user on sys_user.id = mm_material.create_by WHERE mm_material.code = :code AND mm_material.name like :name AND material_type = :materialType AND category_id = :categoryId AND mm_material.is_deleted = 0"
+        String sql = "SELECt mm_material.id, mm_material.code, mm_material.name, specification, case when attachment is null or length(attachment) <= 2 then '无' else '有' end  attachment, material_type, mm_material.category_id, base_unit, standard_price, sys_user.name create_name,DATE_FORMAT(mm_material.create_time, '%Y-%m-%d %H:%i:%s') create_time from mm_material left join sys_user on sys_user.id = mm_material.create_by WHERE mm_material.code = :code AND mm_material.name like :name AND material_type = :materialType AND category_id = :categoryId AND mm_material.is_deleted = 0"
                 .replaceFirst("(?i)(select)\\s+", "SELECT ")
                 .replaceFirst("(?i)\\s+(from)\\s+", " FROM ")
                 .replaceFirst("(?i)\\s+(WHERE)\\s+", " WHERE ");
