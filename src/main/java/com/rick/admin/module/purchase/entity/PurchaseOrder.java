3d52ed4d87aac743a871b5671ace6cd3970c8c3c
rick <jkxyx205@163.com>
2023-12-17 11:18:35 +0800

    fix: bugfix

--- src/main/java/com/rick/admin/module/purchase/entity/PurchaseOrder.java
+++ src/main/java/com/rick/admin/module/purchase/entity/PurchaseOrder.java
@@ -150,25 +150,10 @@ public class PurchaseOrder extends BaseCodeEntity {
         MaterialDescription materialDescription;
 
         @Transient
-        @Sql(value = "select t.*, core_partner.name, core_partner.short_name from (\n" +
-                "select source_order_num       sourceOrderNum,\n" +
-                "       customer_material_code customerMaterialCode,\n" +
-                "       produce_order.id,\n" +
-                "       produce_order.code,\n" +
-                "       produce_order.partner_id\n" +
-                "from produce_order_item,\n" +
-                "     produce_order\n" +
-                "where produce_order_item.produce_order_id = produce_order.`id`\n" +
-                "  AND (produce_order_item.id = :referenceId2)\n" +
-                "union all\n" +
-                "select source_order_num sourceOrderNum,\n" +
-                "       null             customer_material_code,\n" +
-                "       produce_order.id,\n" +
-                "       produce_order.code,\n" +
-                "       produce_order.partner_id\n" +
-                "from produce_order\n" +
-                "where produce_order.id = :referenceId2) t, core_partner\n" +
-                "where core_partner.id = t.partner_id", params = "referenceId2@referenceId2", nullWhenParamsIsNull = {"referenceId2"})
+        @Sql(value = "select source_order_num sourceOrderNum, produce_order.id,\n" +
+                "                       produce_order.code,\n" +
+                "                       produce_order.partner_id,\n" +
+                "                       core_partner.name, core_partner.short_name from pur_purchase_requisition_item, produce_order, core_partner where pur_purchase_requisition_item.id = :referenceId1 and produce_order.id = reference_document_id and core_partner.id = produce_order.partner_id", params = "referenceId1@referenceId1", nullWhenParamsIsNull = {"referenceId1"})
         private Map<String, Object> soInfo;
 
         public BigDecimal getAmount() {
