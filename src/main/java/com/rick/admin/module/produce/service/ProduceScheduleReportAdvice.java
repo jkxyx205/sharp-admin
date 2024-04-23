f7066b2441b3d82cc3937cc671d1e9d603172e49
rick <jkxyx205@163.com>
2024-04-23 15:49:24 +0800

    feat: 生产如果有领料记录，那么「领料记录」变成红色

--- src/main/java/com/rick/admin/module/produce/service/ProduceScheduleReportAdvice.java
+++ src/main/java/com/rick/admin/module/produce/service/ProduceScheduleReportAdvice.java
@@ -2,16 +2,16 @@ package com.rick.admin.module.produce.service;
 
 import com.rick.admin.module.material.service.MaterialProfileService;
 import com.rick.admin.module.material.service.MaterialProfileSupport;
+import com.rick.db.service.SharpService;
+import com.rick.db.service.support.Params;
 import com.rick.report.core.entity.Report;
 import com.rick.report.core.service.ReportAdvice;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -24,6 +24,8 @@ public class ProduceScheduleReportAdvice implements ReportAdvice {
 
     private final MaterialProfileService materialProfileService;
 
+    private final SharpService sharpService;
+
     @Override
     public void beforeSetRow(Report report, List<Map<String, Object>> rows) {
         Set<String> materialIdBatchIdStringCollection = rows.stream().map(row -> MaterialProfileSupport.materialIdBatchIdString((Long) row.get("materialId"), (Long) row.get("batch_id"))).collect(Collectors.toSet());
@@ -35,6 +37,29 @@ public class ProduceScheduleReportAdvice implements ReportAdvice {
             String specification = (String) row.get("specification");
             row.put("specification", specification);
             row.put("specificationAndCharacteristic", (StringUtils.isBlank(specification) ? "" : specification + " ") + Objects.toString(row.get("characteristic"), ""));
+        }
+
+        // 获取已经领过料的生产单号
+        Set<String> scheduleCodes = rows.stream().map(row -> row.get("code").toString()).collect(Collectors.toSet());
+        List<String> haveMaterialScheduleCodes = sharpService.query("select distinct reference_code from inv_document where type = 'MOVING_TO_PRODUCE' AND reference_code in (:scheduleCodes)",
+                Params.builder(1).pv("scheduleCodes", scheduleCodes).build(),
+                String.class);
+
+        if (CollectionUtils.isNotEmpty(haveMaterialScheduleCodes)) {
+            report.getAdditionalInfo().put("js", "const haveMaterialScheduleCodes = '"+String.join(",", haveMaterialScheduleCodes)+"';" +
+                    "$('table td span[name=code]').each(function() {\n" +
+                    "\tif (haveMaterialScheduleCodes.indexOf($(this).text()) > -1) {\n" +
+                    "\t\t$(this).parents('tr').find('.have-material').css('color', 'rgb(248, 108, 107)').css('font-weight', 'bold')\n" +
+                    "\t}\t\n" +
+                    "})");
+        }
+
+    }
+
+    @Override
+    public void init(Report report) {
+        if (report.getAdditionalInfo() == null) {
+            report.setAdditionalInfo(new HashMap<>());
         }
     }
 }
