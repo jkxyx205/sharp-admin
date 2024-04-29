2a72eb2fd6996f6fb20dc868b338fa67a3e4a0e9
rick <jkxyx205@163.com>
2024-04-29 15:20:38 +0800

    chore: 添加采购明细报表

---
+++ src/main/java/com/rick/admin/module/purchase/service/ListDetailReportAdvice.java
@@ -0,0 +1,82 @@
+package com.rick.admin.module.purchase.service;
+
+import com.rick.admin.common.BigDecimalUtils;
+import com.rick.admin.module.material.service.CharacteristicConverter;
+import com.rick.admin.module.material.service.MaterialProfileService;
+import com.rick.admin.module.material.service.MaterialProfileSupport;
+import com.rick.admin.module.material.service.MaterialService;
+import com.rick.report.core.entity.Report;
+import com.rick.report.core.service.ReportAdvice;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * @author Rick.Xu
+ * @date 2024/4/29 10:21
+ */
+@Component
+@RequiredArgsConstructor
+public class ListDetailReportAdvice implements ReportAdvice {
+
+    private final MaterialProfileService materialProfileService;
+
+    private final CharacteristicConverter characteristicConverter;
+
+    private final MaterialService materialService;
+
+    private final PurchaseOrderService purchaseOrderService;
+
+    @Override
+    public void beforeSetRow(Report report, List<Map<String, Object>> rows) {
+        Set<String> materialIdBatchIdStringCollection = rows.stream().map(row -> MaterialProfileSupport.materialIdBatchIdString((Long) row.get("material_id"), (Long) row.get("batch_id"))).collect(Collectors.toSet());
+        Map<String, String> characteristicTextMap = materialProfileService.getCharacteristicText(materialIdBatchIdStringCollection);
+        Set<Long> materialIds = rows.stream().map(row -> (Long) row.get("material_id")).collect(Collectors.toSet());
+        Set<Long> itemIds = rows.stream().map(row -> (Long) row.get("id")).collect(Collectors.toSet());
+
+        Map<Long, BigDecimal> historyGoodsReceiptQuantityMap = purchaseOrderService.historyGoodsReceiptQuantity(itemIds);
+
+        materialService.consumeMaterialDescription(materialIds, (idMaterialMap, dictService) -> {
+            for (Map<String, Object> row : rows) {
+                row.put("materialName", idMaterialMap.get(row.get("material_id")).getName());
+                String characteristic = characteristicTextMap.get(MaterialProfileSupport.materialIdBatchIdString((Long) row.get("material_id"), (Long) row.get("batch_id")));
+                String specification = characteristicConverter.convert(null, (String) row.get("specification"));
+                row.put("specificationAndCharacteristic", (StringUtils.isBlank(specification) ? "" : specification + " ") + Objects.toString(characteristic, ""));
+                row.put("unit_price", ((BigDecimal)row.get("unit_price")).stripTrailingZeros().toPlainString());
+                row.put("total_price", ((BigDecimal)row.get("total_price")).stripTrailingZeros().toPlainString());
+
+                row.put("goodsReceiptQuantity", historyGoodsReceiptQuantityMap.get(row.get("id")).stripTrailingZeros().toPlainString());
+                if (1L == (Long) row.get("is_complete")) {
+                    row.put("openQuantity", "0");
+
+                    if ("0".equals(row.get("goodsReceiptQuantity"))) {
+                        row.put("goodsReceiptQuantity", row.get("quantity"));
+                    }
+                } else {
+                    BigDecimal openQuantity = ((BigDecimal) row.get("quantity")).subtract(historyGoodsReceiptQuantityMap.get(row.get("id")));
+                    row.put("openQuantity", (openQuantity.equals(BigDecimal.ZERO) ? "": "-") + (BigDecimalUtils.lt(openQuantity, BigDecimal.ZERO) ? BigDecimal.ZERO : openQuantity.stripTrailingZeros().toPlainString()));
+                }
+            }
+        });
+    }
+
+    @Override
+    public void init(Report report) {
+        report.getAdditionalInfo().put("js", "$('table tr span[name=is_complete]').each(function() {\n" +
+                "    if ($(this).text() === '是') {\n" +
+                "        $(this).css('color', 'rgb(77, 189, 116)')\n" +
+                "    } else {\n" +
+                "        $(this).css('color', 'rgb(248, 108, 107)')\n" +
+                "    }\n" +
+                "});" +
+                "$('table tr span[name=goodsReceiptQuantity]').parent().css('background-color', 'rgb(77, 189, 116)');\n" +
+                "$('table tr span[name=openQuantity]').parent().css('background-color', 'rgb(248, 108, 107)');");
+    }
+}
--- src/test/java/com/rick/admin/module/PurchaseOrderTest.java
+++ src/test/java/com/rick/admin/module/PurchaseOrderTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 /**
  * @author Rick.Xu
@@ -52,6 +53,49 @@ public class PurchaseOrderTest {
                 .pageable(true)
                 .sidx("id")
                 .sord(SordEnum.DESC)
+                .build());
+    }
+
+    @Test
+    public void testPurchaseDetail() {
+        reportService.saveOrUpdate(Report.builder()
+                .id(816284792017412096L)
+                .code("pur_purchase_order_detail")
+                .tplName("modules/purchase/list-detail")
+                .name("采购明细列表")
+                .reportAdviceName("listDetailReportAdvice")
+                .additionalInfo(new HashMap<>())
+                .querySql("select pur_purchase_order_item.id, pur_purchase_order.partner_id, pur_purchase_order.id order_id, pur_purchase_order.code, pur_purchase_order_item.material_id, pur_purchase_order_item.material_code,\n" +
+                        "pur_purchase_order_item.batch_id, pur_purchase_order_item.batch_code, pur_purchase_order_item.quantity, pur_purchase_order_item.unit, pur_purchase_order_item.unit_price, (pur_purchase_order_item.quantity * pur_purchase_order_item.unit_price) total_price, pur_purchase_order.create_time, pur_purchase_order_item.delivery_date, (pur_purchase_order_item.is_complete + 0) is_complete from pur_purchase_order_item, pur_purchase_order where pur_purchase_order_item.purchase_order_code = pur_purchase_order.code and " +
+                        "code = :code and partner_id = :partner_id and material_id = :material_id and material_code = :material_code and batch_id = :batch_id and is_complete = :is_complete and pur_purchase_order_item.create_time >= :create_time0 and pur_purchase_order_item.create_time <= :create_time1")
+                .queryFieldList(Arrays.asList(
+                        new QueryField("code", "订单号", QueryField.Type.TEXT),
+                        new QueryField("partner_id", "供应商", QueryField.Type.SELECT, "core_partner_vendor"),
+                        new QueryField("is_complete", "完成收货", QueryField.Type.SELECT, "bol"),
+                        new QueryField("create_time", "订单时间", QueryField.Type.DATE_RANGE)
+                ))
+                .reportColumnList(Arrays.asList(
+                        new HiddenReportColumn("id"),
+                        new HiddenReportColumn("order_id"),
+                        new HiddenReportColumn("material_id"),
+                        new ReportColumn("code", "订单号").setColumnWidth(130),
+                        new ReportColumn("partner_id", "供应商", false, "core_partner", Arrays.asList("dictConverter")),
+                        new ReportColumn("material_code", "物料编号"),
+                        new ReportColumn("materialName", "物料名称"),
+                        new ReportColumn("specificationAndCharacteristic", "规格 & 特征值"),
+                        new ReportColumn("quantity", "数量").setType(ReportColumn.TypeEnum.NUMERIC),
+                        new ReportColumn("unit", "单位", false, "unit", Arrays.asList("dictConverter")),
+                        new ReportColumn("unit_price", "含税单价(元)").setType(ReportColumn.TypeEnum.DECIMAL),
+                        new ReportColumn("total_price", "含税总计(元)").setType(ReportColumn.TypeEnum.DECIMAL),
+                        new ReportColumn("delivery_date", "交货日期", false,null, Arrays.asList("sqlDateConverter")).setColumnWidth(120),
+                        new ReportColumn("create_time", "采购时间", false,null, Arrays.asList("localDateTimeConverter")).setColumnWidth(180),
+                        new ReportColumn("is_complete", "收货状态", false, "bol", Arrays.asList("dictConverter")).setColumnWidth(50),
+                        new ReportColumn("goodsReceiptQuantity", "已收货数量"),
+                        new ReportColumn("openQuantity", "待收货数量")
+                ))
+                .pageable(true)
+                .sidx("id")
+                .sord(SordEnum.ASC)
                 .build());
     }
 }
