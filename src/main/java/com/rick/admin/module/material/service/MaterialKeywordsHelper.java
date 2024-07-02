5dcfda4330061f3e76f61fee25a9a54a59e2780d
rick <jkxyx205@163.com>
2024-07-02 10:15:38 +0800

    refactor: 代码优化

--- src/main/java/com/rick/admin/module/material/service/MaterialKeywordsHelper.java
+++ src/main/java/com/rick/admin/module/material/service/MaterialKeywordsHelper.java
@@ -22,8 +22,8 @@ public class MaterialKeywordsHelper {
             for (int i = 0; i < keywordArray.length; i++) {
                 String paramName = "keyword" + i;
                 condition += " AND (mm_material.code like :"+paramName+" or mm_material.name like :"+paramName+" or specification like :"+paramName+" " +
-                        "OR (batch_id in (select mm_batch.id from mm_batch, mm_profile, mm_characteristic_value where\n" +
-                        " mm_batch.profile_id = mm_profile.id AND mm_characteristic_value.reference_id = mm_profile.id AND mm_characteristic_value.value like :"+paramName+")))";
+                        "OR (batch_id in (select mm_profile.batch_id from mm_profile, mm_characteristic_value where\n" +
+                        " mm_characteristic_value.reference_id = mm_profile.id AND mm_characteristic_value.value like :"+paramName+")))";
                 requestMap.put(paramName, keywordArray[i]);
             }
 
